475ebaff99dc1661b17d61b11a4434541f1dce62
tswgit <dztxd@126.com>
2018-06-13 15:09:22 +0800

    update java image

--- java/README.md
+++ java/README.md
@@ -1,3 +1,6 @@
 
 Oracle Jdk jdk-8u171-linux-x64
 Based on centos:7
+
+添加楷体
+
---
+++ java/init.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+set -e
+
+# set locale
+ln -sf /usr/share/zoneinfo/Asia/Shanghai /etc/localtime
+
+# yum install 
+yum install -y openssh-clients fontconfig mkfontscale which lsof net-tools wget fontconfig mkfontscale
+
+
+# jdk
+cd /opt
+wget -c --no-cookies --no-check-certificate --header "Cookie: oraclelicense=accept-securebackup-cookie"  http://download.oracle.com/otn-pub/java/jdk/8u171-b11/512cd62ec5174c3487ac17c61aaa89e8/jdk-8u171-linux-x64.tar.gz
+tar -zxf jdk-8u171-linux-x64.tar.gz 
+rm -f jdk-8u171-linux-x64.tar.gz
+ln -s jdk1.8.0_171/ jdk 
+
+# font
+mkfontscale
+mkfontdir
+fc-cache
+
+rm -f /tmp/init.sh
